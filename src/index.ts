7a9ce0482eb6779a6bf28ec4e6ed58e99ba9ed0c
Yamato Matsuoka <yamaton@gmail.com>
2022-12-21 22:59:14 -0500

    Add 'Copy Relative Path' to the tab context menu

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import { Clipboard } from '@jupyterlab/apputils';
 import { fileIcon } from '@jupyterlab/ui-components';
 
 import { getRelativePath } from './utils';
+import { Widget } from '@lumino/widgets';
+import { find } from '@lumino/algorithm';
 
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'context-menu',
@@ -20,9 +22,10 @@ const extension: JupyterFrontEndPlugin<void> = {
     docManager: IDocumentManager,
     labShell: ILabShell
   ) => {
+    // For File Browser items
     app.commands.addCommand('filebrowser:copy-relative-path', {
       label: 'Copy Relative Path',
-      caption: 'Copy path relative to the active notebook.',
+      caption: 'Copy path relative to the active document.',
       icon: fileIcon.bindprops({ stylesheet: 'menuItem' }),
       execute: () => {
         const widget = factory.tracker.currentWidget;
@@ -44,10 +47,10 @@ const extension: JupyterFrontEndPlugin<void> = {
         }
 
         console.debug(`context.path: ${context.path}`);
+        console.debug(`item.path: ${item.path}`);
         const relativePath = getRelativePath(item.path, context.path);
-
-        Clipboard.copyToSystem(relativePath);
         console.debug(`Copied relative path to clipboard: ${relativePath}`);
+        Clipboard.copyToSystem(relativePath);
       },
       isVisible: () =>
         !!factory.tracker.currentWidget &&
@@ -55,6 +58,63 @@ const extension: JupyterFrontEndPlugin<void> = {
         !!labShell.currentWidget &&
         !!docManager.contextForWidget(labShell.currentWidget),
     });
+
+
+    // [NOTE] borrowed from jupyterlab/packages/application-extension/src/index.tsx
+    const { shell } = app;
+    const contextMenuWidget = (): Widget | null => {
+      const test = (node: HTMLElement) => !!node.dataset.id;
+      const node = app.contextMenuHitTest(test);
+
+      if (!node) {
+        // Fall back to active widget if path cannot be obtained from event.
+        return shell.currentWidget;
+      }
+
+      return (
+        find(shell.widgets('main'), widget => widget.id === node.dataset.id) ||
+        shell.currentWidget
+      );
+    };
+
+    // For tabs
+    app.commands.addCommand('docmanager:copy-relative-path', {
+      label: 'Copy Relative Path',
+      caption: 'Copy path relative to the active document',
+      icon: fileIcon.bindprops({ stylesheet: 'menuItem' }),
+      execute: () => {
+        const targetWidget = contextMenuWidget();
+        if (!targetWidget) {
+          return;
+        }
+        console.debug('widget:', targetWidget);
+
+        const targetContext = docManager.contextForWidget(targetWidget);
+        if (!targetContext) {
+          return;
+        }
+
+        if (!labShell.currentWidget) {
+          return;
+        }
+
+        console.debug('labShell.currentWidget:', labShell.currentWidget);
+        const currentContext = docManager.contextForWidget(labShell.currentWidget);
+        if (!currentContext) {
+          return;
+        }
+
+        console.debug(`(current) context.path: ${currentContext.path}`);
+        console.debug(`(target)  context.path: ${targetContext.path}`);
+        const relativePath = getRelativePath(targetContext.path, currentContext.path);
+        console.debug(`Copied relative path to clipboard: ${relativePath}`);
+        Clipboard.copyToSystem(relativePath);
+      },
+      isVisible: () =>
+        !!labShell.currentWidget &&
+        !!docManager.contextForWidget(labShell.currentWidget),
+    });
+
   },
 };
 
